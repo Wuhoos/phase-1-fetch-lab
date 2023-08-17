dcaa5be389ec9782d4226f45b61293a57feb7e81
David <wjw3298@gmail.com>
2023-08-17 14:20:38 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 function fetchBooks() {
   fetch('https://anapioficeandfire.com/api/books')
   .then(response => response.json())
-  .then(books => {
-    renderBooks(books)
-  })
-  
+  .then(books => renderBooks(books))
 }
 
 function renderBooks(books) {
